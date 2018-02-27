dc204251799b71a572f8c8e14a74f488b33b75bb
Eric McKinney <mckinney99@gmail.com>
2018-02-27 17:41:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
    this.name = name
    this.homeState = homeState
    this.training = training
-}
+   }
    BoardMember.prototype.veto = function(){
      return 'No, I must disagree'
    }
    BoardMember.prototype.approve = function(){
      return 'You can do that!'
    }
-   BoardMember.prototype.doCharity = function(){     
+   BoardMember.prototype.doCharity = function(){
      return "I like to help people."
    }
 
